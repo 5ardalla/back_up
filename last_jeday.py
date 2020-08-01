5680c75e39adb273ed824b286df88125da703f7e
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2020-08-01 21:13:04 +0200

    Update last_jeday.py

--- last_jeday.py
+++ last_jeday.py
@@ -76,8 +76,9 @@ def check_web(driver):
 #		eto_firstName.send_keys(Keys.F11)
 		#eto_firstName.click()
 		print("web page working sleep 10")
-		driver.refrech()
+		#driver.refrech()
 		time.sleep(30)
+		driver.refrech()
 		check_web(driver)
 
 	except:
